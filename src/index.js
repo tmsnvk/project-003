6a53aa7b575e50f42f7b92932add18b1342cf4f5
tmsnvk <57031664+tmsnvk@users.noreply.github.com>
2020-03-31 08:31:00 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import - functions and styles;
+import "./styles/base.scss";
+import "./styles/grid.scss";
+import "./styles/navbar.scss";
+import "./styles/footer.scss";
+import "./styles/page_main.scss";
+import "./styles/page_post.scss";
+import "./styles/mediaq.scss";
