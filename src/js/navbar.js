0fc7e4434bb0ca148c5e58d7145c7f4d3232cbd4
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-12 14:42:58 +0200

    Add files via upload

---
+++ src/js/navbar.js
@@ -0,0 +1,19 @@
+// h1 @navbar collapse
+const header = document.querySelector("h1");
+const headerHeight = header.offsetHeight;
+
+const addClass = () => header.classList.add("heading-transform");
+const removeClass = () => header.classList.remove("heading-transform");
+
+function toggleClass() {
+ let scrollPosition = window.scrollY;
+
+ if (scrollPosition >= headerHeight) { 
+  addClass();
+ } 
+ else { 
+  removeClass();
+ }
+}
+
+export { toggleClass }
