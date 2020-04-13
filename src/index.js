3fd9256c21f277bccf8564c14e735c6922d0d081
tamas_nvk <57031664+tmsnvk@users.noreply.github.com>
2020-04-13 16:48:24 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-// import - functions and styles;
+// import css styles;
 import "./styles/base.scss";
-import "./styles/grid.scss";
+import "./styles/grid_home.scss";
 import "./styles/navbar.scss";
 import "./styles/footer.scss";
-import "./styles/page_main.scss";
-import "./styles/page_post.scss";
+import "./styles/page_home.scss";
 import "./styles/mediaq.scss";
+
+// import fontawesome;
+import '@fortawesome/fontawesome-free/js/fontawesome.js'
+import '@fortawesome/fontawesome-free/js/solid.js'
+
+// import js;
+import { toggleClass } from "./js/navbar.js"; 
+import { getPokemonImage } from "./js/imageholder.js";
+import { buttonScroll } from "./js/button.js";
+
+// scrolling;
+window.addEventListener("scroll", toggleClass, false);
+
+// to insert random pokemon images into image-holder divs;
+getPokemonImage();
+
+// button
+buttonScroll();
